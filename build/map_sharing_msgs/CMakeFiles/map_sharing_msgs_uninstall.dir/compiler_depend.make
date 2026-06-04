97aa62fcb916a477825c9b4851139a3a52dc444c
natchuop <nathan.chuop.work@gmail.com>
2026-06-04 15:43:02 -0700

    Initial ROS2 Project Structure

---
+++ build/map_sharing_msgs/CMakeFiles/map_sharing_msgs_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for map_sharing_msgs_uninstall.
+# This may be replaced when dependencies are built.
---
+++ build/robot_patrol_node/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/natch/projects/robot-map-poisoning-defense/install/robot_patrol_node'
